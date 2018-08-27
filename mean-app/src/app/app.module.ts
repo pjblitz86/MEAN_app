536e50db2914ad9d625eb72a508aafcb765e52bc
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-27 15:50:13 +0300

    adding Angular Material and some input and navbar

--- mean-app/src/app/app.module.ts
+++ mean-app/src/app/app.module.ts
@@ -1,6 +1,9 @@
+import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -8,11 +11,17 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
