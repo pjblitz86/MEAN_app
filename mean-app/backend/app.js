17f3a8f8377fd28d569021715922773b76749f85
Paulius Juknis <paulius.juknis@gmail.com>
2018-08-30 16:08:38 +0300

    created edit form, edit and update post features implemented

--- mean-app/backend/app.js
+++ mean-app/backend/app.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://pjblitz86:pjblitz86@cluster0-c1rev.mongodb.net/node-angular?retryWrites=true', { useNewUrlParser: true })
+mongoose
+  .connect(
+    'mongodb+srv://pjblitz86:pjblitz86@cluster0-c1rev.mongodb.net/node-angular?retryWrites=true',
+    { useNewUrlParser: true }
+  )
   .then(() => {
     console.log('Connected to db');
   })
@@ -18,19 +22,13 @@ app.use(bodyParser.json());
 
 // CORS allow setup
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT, OPTIONS');
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
+app.post('/api/posts', (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
@@ -43,14 +41,34 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 
+app.put('/api/posts/:id', (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    res.status(200).json({ message: 'Update successful' });
+  });
+});
+
 app.get('/api/posts', (req, res, next) => {
-  Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'Post fetched successfully',
-        posts: documents
-      });
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Post fetched successfully',
+      posts: documents
     });
+  });
+});
+
+app.get('/api/posts/:id', (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Post not found!' });
+    }
+  });
 });
 
 app.delete('/api/posts/:id', (req, res, next) => {
