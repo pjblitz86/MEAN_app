fc24cbc6b5f678644b7073846c8625b75ed313bd
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-04 11:23:45 +0300

    adding the signup form

--- mean-app/src/app/app-routing.module.ts
+++ mean-app/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { SignupComponent } from './auth/signup/signup.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -8,7 +9,8 @@ const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: PostCreateComponent },
   { path: 'edit/:postId', component: PostCreateComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
--- mean-app/src/app/auth/login/login.component.ts
+++ mean-app/src/app/auth/login/login.component.ts
@@ -8,7 +8,5 @@ import { NgForm } from '@angular/forms';
 export class LoginComponent {
   isLoading = false;
 
-  onLogin(form: NgForm) {
-    console.log(form.value);
-  }
+  onLogin(form: NgForm) {}
 }
