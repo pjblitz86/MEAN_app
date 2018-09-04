73502bf872276533faff77e3e58c1214a6a8666b
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-04 08:55:17 +0300

    started adding pagination using built-in angular material paginator

--- mean-app/src/app/app.module.ts
+++ mean-app/src/app/app.module.ts
@@ -9,7 +9,8 @@ import {
   MatButtonModule,
   MatToolbarModule,
   MatExpansionModule,
-  MatProgressSpinnerModule
+  MatProgressSpinnerModule,
+  MatPaginatorModule
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -31,7 +32,8 @@ import { HttpClientModule } from '@angular/common/http';
     MatToolbarModule,
     MatExpansionModule,
     MatProgressSpinnerModule,
-    HttpClientModule
+    HttpClientModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
