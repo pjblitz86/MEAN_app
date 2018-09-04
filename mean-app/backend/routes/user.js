fd5b448427e186a67e41a0c20e74acd5e3413401
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-04 14:22:18 +0300

    creating a new user upon request implementation mapping to db

--- mean-app/backend/routes/user.js
+++ mean-app/backend/routes/user.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
-router.post('/signup', (req, res, next) => {});
+router.post('/signup', (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash // careful with this - must be hashed, not raw
+    });
+    user
+      .save()
+      .then(result => {
+        res.status(200).json({
+          message: 'User created',
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(201).json({
+          error: err
+        });
+      });
+  });
+});
 
 module.exports = router;
