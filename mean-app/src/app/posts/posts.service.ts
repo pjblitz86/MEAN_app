354661190755a00fa7bd5e4881ecdbc68d243d2d
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-06 09:23:16 +0300

    using userId on the frontend

--- mean-app/src/app/posts/posts.service.ts
+++ mean-app/src/app/posts/posts.service.ts
@@ -46,7 +46,7 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{ _id: string; title: string; content: string; imagePath: string }>(
+    return this.http.get<{ _id: string; title: string; content: string; imagePath: string, creator: string }>(
       'http://localhost:3000/api/posts/' + id
     );
   }
@@ -76,7 +76,8 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http.put('http://localhost:3000/api/posts/' + id, postData).subscribe(response => {
