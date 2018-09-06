d7add168c5c2d7a67a65cebbeea1ec1f94f1461f
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-06 11:37:36 +0300

    testing different places to handle errors in login and signup

--- mean-app/src/app/auth/signup/signup.component.ts
+++ mean-app/src/app/auth/signup/signup.component.ts
@@ -1,15 +1,27 @@
 import { AuthService } from '../auth.service';
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit, OnDestroy {
   isLoading = false;
+  private authStatusSub: Subscription;
 
   constructor(public authService: AuthService) {}
+
+  ngOnInit() {
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(authStatus => {
+      this.isLoading = false;
+    });
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
+  }
 
   onSignup(form: NgForm) {
     if (form.invalid) {
