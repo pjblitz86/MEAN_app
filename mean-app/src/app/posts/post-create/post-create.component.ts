07d7f79795f06f323cb0a31ae6604f18c85d5e4a
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-06 15:28:50 +0300

    adding more error handling stuff

--- mean-app/src/app/posts/post-create/post-create.component.ts
+++ mean-app/src/app/posts/post-create/post-create.component.ts
@@ -1,16 +1,18 @@
+import { AuthService } from './../../auth/auth.service';
 import { mimeType } from './mime-type.validator';
 import { Post } from './../post.model';
 import { PostsService } from './../posts.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent implements OnInit {
+export class PostCreateComponent implements OnInit, OnDestroy {
   enteredContent = '';
   enteredTitle = '';
   post: Post;
@@ -19,10 +21,18 @@ export class PostCreateComponent implements OnInit {
   imagePreview: string;
   private mode = 'create';
   private postId: string;
+  private authStatusSub: Subscription;
 
-  constructor(public postsService: PostsService, public route: ActivatedRoute) {}
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(authStatus => {
+      this.isLoading = false;
+    });
     this.form = new FormGroup({
       title: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]
@@ -48,7 +58,6 @@ export class PostCreateComponent implements OnInit {
             content: postData.content,
             imagePath: postData.imagePath,
             creator: postData.creator
-
           };
           this.form.setValue({
             title: this.post.title,
@@ -61,6 +70,10 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 
   onImagePicked(event: Event) {
