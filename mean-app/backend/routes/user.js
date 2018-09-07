14a4e446c2ea019addd65526a87646cc1c5dad9a
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-07 09:27:27 +0300

    refactoring backend/routes folder moving the callbacks to controller folder

--- mean-app/backend/routes/user.js
+++ mean-app/backend/routes/user.js
@@ -1,65 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const UserController = require('../controllers/user');
 
-router.post('/signup', (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then(hash => {
-    const user = new User({
-      email: req.body.email,
-      password: hash // careful with this - must be hashed, not raw, I'm using bcrypt
-    });
-    user
-      .save()
-      .then(result => {
-        res.status(201).json({
-          message: 'User created',
-          result: result
-        });
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: 'Invalid authentication credentials'
-        });
-      });
-  });
-});
+router.post('/signup', UserController.createUser);
 
-router.post('/login', (req, res, next) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({
-          message: 'Auth failed'
-        });
-      }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then(result => {
-      if (!result) {
-        return res.status(401).json({
-          message: 'Auth failed'
-        });
-      }
-      const token = jwt.sign(
-        { email: fetchedUser.email, userId: fetchedUser._id },
-        'secret_this_should_be_longer',
-        { expiresIn: '1h' }
-      );
-      res.status(200).json({
-        token: token,
-        expiresIn: 3600, // duration in seconds
-        userId: fetchedUser._id
-      });
-    })
-    .catch(err => {
-      return res.status(401).json({
-        message: 'Invalid authentication credential'
-      });
-    });
-});
+router.post('/login', UserController.userLogin);
 
 module.exports = router;
